8c431d96091b5d940a1fd8b7dc9226fc9656e680
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-06-14 18:00:15 +0545

    updated structure

---
+++ reddit/src/pages/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Homepage() {
+    return (
+        <div>
+            <header>
+                Navbar
+            </header>
+            <div>
+                Content
+            </div>
+            <footer>
+                Footer
+            </footer>
+        </div>
+    )
+}
