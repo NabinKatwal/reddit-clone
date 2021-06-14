a0d2797b032777b39457b5dc7dab1b3cc459e61a
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-06-14 20:01:29 +0545

    added server code

--- reddit/backend/server.js
+++ reddit/backend/server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
+const router = express.Router()
 const port = 4000 
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res)=>{
-    console.log("Hello world")
+    res.send("Home page")
 })
 
 app.listen(port, ()=>{
